c140a9f791e08f3753eeb4d2e445f6615fd0e5e2
Apathy <apathyhill@gmail.com>
2019-11-19 17:00:18 -0500

    a
    a

--- POSTHEREAPI/app.py
+++ POSTHEREAPI/app.py
@@ -1,11 +1,8 @@
 from decouple import config
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from praw import Reddit
 from urllib.parse import quote_plus
 
-REDDIT = Reddit(client_id=config("CLIENT_ID"),
-                     client_secret=config("CLIENT_SECRET"),
-                     user_agent=config("USER_AGENT"))
 
 def create_app():
     app = Flask(__name__)
@@ -14,13 +11,11 @@ def create_app():
     def root():
         return render_template("home.html")
 
-
-    @app.route("/predict", methods=["POST"])
+    @app.route("/post_to_reddit", methods=["POST"])
     def predict_subreddit():
         if request.method == "POST":
             title = request.values["title"]
-            # predictions = process_title(title)
-            predictions = [{"name": "me_irl", "confidence": 100 }]
-            return render_template("predictions.html", predictions=predictions, title=title, title_url=quote_plus(title))
-        return "ERROR"
+            subreddit = request.values["subreddit"]
+            return redirect("https://www.reddit.com/r/{}/submit?title={}".format(subreddit, quote_plus(title)))
+
     return app

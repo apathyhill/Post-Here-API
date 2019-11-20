f48828eb6608015e743818d454defb86cebbd986
Apathy <apathyhill@gmail.com>
2019-11-20 16:28:16 -0500

    Update db_model.py

--- POSTHEREAPI/db_model.py
+++ POSTHEREAPI/db_model.py
@@ -5,5 +5,5 @@ from flask_sqlalchemy import SQLAlchemy
 DB = SQLAlchemy()
 
 class User(DB.Model):
-    username = DB.Column(Text(), nullable=False)
+    username = DB.Column(DB.Text(), nullable=False)
     password = DB.Column(DB.Text(), nullable=False)

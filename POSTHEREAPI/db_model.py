2b829cea42686072e68f29228b5d917600d341fe
Apathy <apathyhill@gmail.com>
2019-11-21 12:46:17 -0500

    Update db_model.py

--- POSTHEREAPI/db_model.py
+++ POSTHEREAPI/db_model.py
@@ -7,4 +7,4 @@ DB = SQLAlchemy()
 class User(DB.Model):
     username = DB.Column(DB.Text(), nullable=False, primary_key=True)
     password = DB.Column(DB.Text(), nullable=False)
-    session_key = DB.Column(DB.Text(32))
+    session_key = DB.Column(DB.Text())
